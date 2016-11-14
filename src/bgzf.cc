3f68ea2ba2e3603144b157d701e65586eb5c1fd4
Matthew Ralston <matthew.ralston@bms.com>
2016-11-14 15:00:43 -0500

    Fixing GCC 4.9.0 and NAN v2.4 issues

--- src/bgzf.cc
+++ src/bgzf.cc
@@ -1,5 +1,6 @@
 #include <node.h>
 #include <nan.h>
+#include <node_buffer.h>
 
 #include <zlib.h>
 #include <cstring>
@@ -13,23 +14,27 @@ using v8::Object;
 using v8::String;
 using v8::Number;
 using v8::Local;
-using node::Buffer;
-
-#define ERR(msg) v8::ThrowException(v8::Exception::Error(String::New(msg)));
+using namespace node::Buffer;
 
 // function to export
-NAN_METHOD(InflateRawSync) {
-  NanScope();
 
-  if(args.Length() < 2 || !Buffer::HasInstance(args[0]) || !Buffer::HasInstance(args[1])) {
-    return ERR("arguments must be Buffer objects")
+void inflateRawSync(const Nan::FunctionCallbackInfo<v8::Value>& info){
+  Nan::HandleScope scope;
+
+  if(info.Length() < 2){
+    Nan::ThrowTypeError("Wrong number of arguments. Requires 2 buffer objects");
+    return;
   }
-  Local<Object> input  = args[0]->ToObject();
-  Local<Object> result = args[1]->ToObject();
-  Bytef* in_buf  = (Bytef*)Buffer::Data(input);
-  Bytef* res_buf = (Bytef*)Buffer::Data(result);
-  int in_len  = Buffer::Length(input);
-  int res_len = Buffer::Length(result);
+  if (!node::Buffer::HasInstance(info[0]) || !node::Buffer::HasInstance(info[1])) {
+    Nan::ThrowTypeError("One or more arguments has invalid type. Requires 2 buffer objects");
+    return;
+  }
+  Local<Object> input  = info[0]->ToObject();
+  Local<Object> result = info[1]->ToObject();
+  Bytef* in_buf  = (Bytef*)node::Buffer::Data(input);
+  Bytef* res_buf = (Bytef*)node::Buffer::Data(result);
+  int in_len  = node::Buffer::Length(input);
+  int res_len = node::Buffer::Length(result);
 
   z_stream strm;
   strm.zalloc = Z_NULL;
@@ -39,8 +44,10 @@ NAN_METHOD(InflateRawSync) {
   strm.next_in = Z_NULL;
 
   int ret = inflateInit2(&strm, -15);
-  if (ret != Z_OK)
-    return ERR("could not initialize the data");
+  if (ret != Z_OK){
+    Nan::ThrowError("Could not decompress from buffer using zlib!");
+    return;
+  }
 
   strm.avail_in = in_len;
   strm.next_in = in_buf;
@@ -48,17 +55,21 @@ NAN_METHOD(InflateRawSync) {
   strm.next_out = res_buf;
 
   ret = inflate(&strm, Z_NO_FLUSH);
-  if (ret != Z_STREAM_END && ret != Z_OK)
-    return ERR("Invalid format");
+  if (ret != Z_STREAM_END && ret != Z_OK){
+    Nan::ThrowError("Could not decompress completely from buffer using zlib!");
+    return;
+  }
+    
 
   (void)inflateEnd(&strm);
-  NanReturnValue(NanNew<Number>(res_len - strm.avail_out));
+
+  info.GetReturnValue().Set(Nan::New<Number>(res_len - strm.avail_out));
 }
 
 // exposing
-void InitAll(Handle<Object> exports) {
-  exports->Set(NanNew<String>("inflateRawSync"),
-    NanNew<FunctionTemplate>(InflateRawSync)->GetFunction());
+void Init(v8::Local<v8::Object> exports){
+  exports->Set(Nan::New("inflateRawSync").ToLocalChecked(),
+	       Nan::New<v8::FunctionTemplate>(inflateRawSync)->GetFunction());
 }
 
-NODE_MODULE(bgzf, InitAll)
+NODE_MODULE(bgzf, Init)
